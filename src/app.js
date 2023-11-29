de38463c3cbf0cf2ba646bec7bd73fc7c070c2a2
Blackstars <nelson.64@live.fr>
2023-11-29 16:12:15 +0100

    put user

--- src/app.js
+++ src/app.js
@@ -9,9 +9,11 @@ const usersControllers = require("./controllers/usersControllers");
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.post("/api/movies", movieControllers.postMovie);
+app.put("/api/movies/:id", movieControllers.updateMovie);
 
 app.get("/api/users", usersControllers.getUsers);
 app.get("/api/users/:id", usersControllers.getUsersById);
 app.post("/api/users", usersControllers.postUser);
+app.put("/api/users/:id", usersControllers.updateUser);
 
 module.exports = app;
